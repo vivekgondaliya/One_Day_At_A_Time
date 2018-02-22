727047caea2cad59c31597ed84940b277800fe1a
vivekgondaliya <vg13@zips.uakron.edu>
2018-02-22 11:45:27 -0800

    More higher order functions - map and reduce

--- Higher Order Functions/higherOrderFunctions.js
+++ Higher Order Functions/higherOrderFunctions.js
@@ -1,3 +1,9 @@
+/**
+    Higher-Order Function : a function that accepts another function as an argument
+
+    Concept of Functional Programming: Less bugs with less development time. Functions are 
+    values in JS. 
+ */
 var animals = [
     { name: ‘Waffles’, type: ‘dog’, age: 12 },
     { name: ‘Fluffy’, type: ‘cat’, age: 14 },
@@ -20,3 +26,21 @@ var oldDogs = animals.filter(function(animal){
 // oldDogs will now be an array that contain only Waffles and Hank objects.
 
 
+//higher-order funciton: map
+var oldDogNames = 
+animals
+  .filter(function(animal) {
+    return animal.age > 10 && animal.type === 'dog';
+  })
+  .map(function(animal) {
+    return animal.name;
+  });
+// oldDogNames will now be the array [ ‘Waffles’, ‘Hank’ ].
+
+//higher-order funciton: reduce with aarow function syntax
+var totalDogYears = animals
+.filter((x) => x.type === 'dog')
+.map   ((x) => x.age)
+.reduce((prev, cur) => { prev + cur }, 0);
+// totalDogYears will be the integer 27 (Because Waffles 12 +  
+// Spelunky 4 + Hank 11)
