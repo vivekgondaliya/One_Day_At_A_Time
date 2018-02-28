b64cc5795d4861463178c23b0bda7b0e923bfaec
vivekgondaliya <vg13@zips.uakron.edu>
2018-02-27 23:58:44 -0800

    Pass by Reference - Arrays

--- Objects and Arrays - Ref vs Copy /index.js
+++ Objects and Arrays - Ref vs Copy /index.js
@@ -12,10 +12,20 @@ name = 'Maulik';
 console.log(name, name2);
 
 //arrays
-
-//make a copy of it
+let array = ['One', 'Two', 'Three']
+let array2 = array;
+console.log(array, array2);
+array[3]= 'Four';
+console.log(array, array2);
 
 //how to fix the issue with arrays
+const arraySlice = array.slice();
+const arrayConcat = [].concat(array);
+const arrayES6 = [...array];            //preferred
+const arrayFrom = Array.from(array);    //preferred
+
+array[4] = 'Five';
+console.log(array, arraySlice, arrayConcat, arrayES6, arrayFrom);
 
 //objects
 
