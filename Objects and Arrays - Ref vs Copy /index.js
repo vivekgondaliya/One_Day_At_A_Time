d3ee929e08431ece0aadad28868dc1c9f58da724
vivekgondaliya <vg13@zips.uakron.edu>
2018-02-27 23:50:49 -0800

    Pass by Copy - String, Numbers and Booleans

---
+++ Objects and Arrays - Ref vs Copy /index.js
@@ -0,0 +1,24 @@
+//start with strings, numbers and booleans
+let age = 25;
+let age2 = age;
+console.log(age, age2);
+age = 18;
+console.log(age, age2);
+
+let name = 'Vivek';
+let name2 = name;
+console.log(name, name2);
+name = 'Maulik';
+console.log(name, name2);
+
+//arrays
+
+//make a copy of it
+
+//how to fix the issue with arrays
+
+//objects
+
+//make a copy of it
+
+//poor man's deep clone
